cb9983cf13b25e53d77794c62fdd58fcae5335ca
Renat.Amirov <renat.amirov@juspay.in>
2020-11-24 21:56:08 +0400

    getAllAccounts and getAllAccounts works together.

--- ak-test1/src/Lib.hs
+++ ak-test1/src/Lib.hs
@@ -38,8 +38,8 @@ $(deriveJSON defaultOptions ''Account)
 $(deriveJSON defaultOptions ''Token)
 
 type API = 
-              -- "accounts" :> "all"                           :> Get    '[JSON] [Account]
-         "accounts" :> Capture "id" Int                :> Get    '[JSON] Account
+              "accounts" :> "all"                           :> Get    '[JSON] [Account]
+         :<|> "accounts" :> Capture "id" Int                :> Get    '[JSON] Account
       -- :<|> "accounts" :> ReqBody '[JSON] Account         :> Post   '[JSON] Account
       -- :<|> "accounts" :> Capture "accountId" Int         :> Put    '[JSON] Account
       -- :<|> "accounts" :> Capture "accountId" Int         :> DeleteNoContent
@@ -61,8 +61,8 @@ api = Proxy
 
 server :: Server API
 server = 
-            -- liftIO getAllAccounts
-       liftIO . getAccountById
+            liftIO   getAllAccounts
+       :<|> liftIO . getAllAccounts
     -- :<|> createAccount
     -- :<|> updateAccountById
     -- :<|> deleteAccountById
@@ -75,13 +75,12 @@ server =
 
   where
 
-    -- getAllAccounts :: IO [Account]
-    -- getAllAccounts = do
-    --   dbRaw <- readFile "./Database.txt"
-    --   let db = read dbRaw
-    --   let accts = accounts db
-    --   return accts
-
+    getAllAccounts :: IO [Account]
+    getAllAccounts = do
+      dbRaw <- readFile "./Database.txt"
+      let db = read dbRaw
+      let accts = accounts db
+      return accts
 
     getAccountById :: Int -> IO Account
     getAccountById id = do
